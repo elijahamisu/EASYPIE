9c9ff4680eee2bec339baa3cd39c98f82ee7242e
elijahamisu <amisuelijaholuwasegun@gmail.com>
2026-07-29 03:06:39 +0100

    Uncomment earnings.html in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
         wallet: resolve(__dirname, 'wallet.html'),
         deposit: resolve(__dirname, 'deposit.html'),
         withdraw: resolve(__dirname, 'withdraw.html'),
-      //  earnings: resolve(__dirname, 'earnings.html'),
+        earnings: resolve(__dirname, 'earnings.html'),
        // transactions: resolve(__dirname, 'transactions.html'),
        // referrals: resolve(__dirname, 'referrals.html'),
       //  notifications: resolve(__dirname, 'notifications.html'),
