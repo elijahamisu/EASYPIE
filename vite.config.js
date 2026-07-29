2520e16ff4990764dc060d2259a96bade5d77bf4
elijahamisu <amisuelijaholuwasegun@gmail.com>
2026-07-29 03:42:09 +0100

    Uncomment profile route in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
         transactions: resolve(__dirname, 'transactions.html'),
         referrals: resolve(__dirname, 'referrals.html'),
         notifications: resolve(__dirname, 'notifications.html'),
-      ////  profile: resolve(__dirname, 'profile.html'),
+        profile: resolve(__dirname, 'profile.html'),
        // support: resolve(__dirname, 'support.html'),
       //  terms: resolve(__dirname, 'terms.html'),
         giftcode: resolve(__dirname, 'giftcode.html'),
