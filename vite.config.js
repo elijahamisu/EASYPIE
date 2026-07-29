dc8dd25559f1a4e1cfbd7dc07913c6234ad55417
elijahamisu <amisuelijaholuwasegun@gmail.com>
2026-07-29 02:55:10 +0100

    Uncomment deposit route in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
         plans: resolve(__dirname, 'plans.html'),
         investment: resolve(__dirname, 'investment.html'),
         wallet: resolve(__dirname, 'wallet.html'),
-       // deposit: resolve(__dirname, 'deposit.html'),
+        deposit: resolve(__dirname, 'deposit.html'),
       //  withdraw: resolve(__dirname, 'withdraw.html'),
       //  earnings: resolve(__dirname, 'earnings.html'),
        // transactions: resolve(__dirname, 'transactions.html'),
