a47294a1ea4b4953c13d39cebb9b8931a727b05f
elijahamisu <amisuelijaholuwasegun@gmail.com>
2026-07-29 02:18:48 +0100

    Uncomment dashboard input in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         login: resolve(__dirname, 'login.html'),
         register: resolve(__dirname, 'register.html'),
-        // dashboard: resolve(__dirname, 'dashboard.html'), 
+        dashboard: resolve(__dirname, 'dashboard.html'), 
       },
     },
   },
